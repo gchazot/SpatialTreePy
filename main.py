18d8da099e1191c9cddea8ef32eda902b99fbe99
Germain CHAZOT <g.chazot@gmail.com>
2017-11-26 19:49:42 -0500

    Project init

---
+++ main.py
@@ -0,0 +1,91 @@
+import fileinput
+import sys
+from spatial_tree import Point, Rectangle, SpatialTree
+
+
+def debug(*args, **kwargs):
+    kwargs.update(file=sys.stderr)
+    return print(*args, **kwargs)
+
+
+def get_files():
+    yield "code_challenge/20170901_080005.csv"
+
+
+def distance(latitude_1, longitude_1, latitude_2, longitude_2):
+    from math import radians, cos, sin, asin, sqrt
+
+    EARTH_RADIUS_KM = 6371.0
+    lat_1_rad = radians(latitude_1)
+    lon_1_rad = radians(longitude_1)
+    lat_2_rad = radians(latitude_2)
+    lon_2_rad = radians(longitude_2)
+    u = sin( (lat_2_rad - lat_1_rad) / 2 )
+    v = sin( (lon_2_rad - lon_1_rad) / 2 )
+
+    return 2 * EARTH_RADIUS_KM * asin( sqrt( u**2 + cos(lat_1_rad) * cos(lat_2_rad) * (v**2) ) )
+
+
+class Flight(Point):
+    def __init__(self, call_sign, latitude, longitude):
+        super().__init__(latitude, longitude)
+        self.call_sign = call_sign
+
+    @staticmethod
+    def create(line):
+        fields = line.split(',')
+        try:
+            lat = float(fields[1])
+            lon = float(fields[2])
+        except ValueError:
+            debug("Not a flight: " + line)
+            return None
+        return Flight(fields[0], lat, lon)
+
+    def distance(self, other):
+        return distance(self.x, self.y, other.x, other.y)
+
+
+def solution1():
+    flights = []
+    for line in fileinput.input(files=get_files()):
+        flight = Flight.create(line)
+        if flight is None:
+            continue
+        flights.append(flight)
+
+    for f1 in flights:
+        closest_flight = None
+        closest_distance = None
+        for f2 in flights:
+            if f1 == f2:
+                continue
+            dist = f1.distance(f2)
+            if closest_distance is None or dist < closest_distance:
+                closest_distance = dist
+                closest_flight = f2
+        print("{} {} {}km".format(f1.call_sign, closest_flight.call_sign, closest_distance))
+
+
+def solution2():
+    index = SpatialTree(Rectangle(-90.0, 90.0, -180.0, 180.0))
+    for line in fileinput.input(files=get_files()):
+        flight = Flight.create(line)
+        if flight is None:
+            continue
+        index.add(flight)
+
+    debug(index)
+    debug(index.count())
+    debug(index.max_count())
+    debug(index.max_depth())
+
+    i = 0
+    for _ in index:
+        i += 1
+    debug(i)
+
+
+if __name__ == "__main__":
+    solution2()
+
---
+++ spatial_tree.py
@@ -0,0 +1,133 @@
+class OutOfBounds(Exception):
+    pass
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Rectangle:
+    def __init__(self, left, right, bottom, top):
+        assert left < right
+        assert bottom < top
+        self.left = left
+        self.right = right
+        self.bottom = bottom
+        self.top = top
+
+    def init_from(self, other):
+        self.left = other.left
+        self.right = other.right
+        self.bottom = other.bottom
+        self.top = other.top
+
+    def __str__(self):
+        return "{bottom}:{left}/{top}:{right}".format(**self.__dict__)
+
+    def __contains__(self, point):
+        return (self.left <= point.x < self.right and
+                self.bottom <= point.y < self.top)
+
+    def centric_split(self):
+        """Splits a rectangle in 4 equal rectangles
+        @:return a list of the new rectangles"""
+        middle_x = (self.right + self.left) / 2.0
+        middle_y = (self.top + self.bottom) / 2.0
+        return [
+            Rectangle(self.left, middle_x, self.bottom, middle_y),
+            Rectangle(self.left, middle_x, middle_y, self.top),
+            Rectangle(middle_x, self.right, self.bottom, middle_y),
+            Rectangle(middle_x, self.right, middle_y, self.top)
+            ]
+
+
+class SpatialLeaf:
+    def __init__(self, bounds, max_items):
+        self.bounds = bounds
+        self.points = []
+        self.max_items = max_items
+
+    def __str__(self):
+        return "Leaf [{}] {}/{}".format(self.bounds, self.count(), self.max_items)
+
+    def add(self, point):
+        if point not in self.bounds:
+            raise OutOfBounds()
+        self.points.append(point)
+
+    def __contains__(self, point):
+        return point in self.bounds
+
+    def split(self):
+        new_leaves = [SpatialLeaf(r, self.max_items) for r in self.bounds.centric_split()]
+        for point in self.points:
+            for leaf in new_leaves:
+                if point in leaf:
+                    leaf.add(point)
+                    break
+        return new_leaves
+
+    def must_split(self):
+        return self.count() > self.max_items
+
+    def count(self):
+        return len(self.points)
+
+    def max_count(self):
+        return self.count()
+
+    def max_depth(self):
+        return 1
+
+    def __iter__(self):
+        return self.points.__iter__()
+
+
+class SpatialTree:
+    def __init__(self, bounds, max_items_per_leaf=10, leaves=None):
+        self.bounds = bounds
+        if leaves is None:
+            leaves = SpatialLeaf(self.bounds, max_items_per_leaf).split()
+        self.leaves = leaves
+
+    def __str__(self):
+        return "Tree [{}]".format(self.bounds)
+
+    def add(self, point):
+        split = None
+        for i, leaf in enumerate(self.leaves):
+            if point in leaf:
+                leaf.add(point)
+                if leaf.must_split():
+                    split = i
+                break
+
+        if split is not None:
+            new_leaf = self.split_of(self.leaves[split])
+            self.leaves[split] = new_leaf
+
+    def __contains__(self, point):
+        return point in self.bounds
+
+    @staticmethod
+    def split_of(leaf):
+        return SpatialTree(leaf.bounds, leaf.max_items, leaf.split())
+
+    def must_split(self):
+        return False
+
+    def count(self):
+        return sum((leave.count() for leave in self.leaves))
+
+    def max_count(self):
+        return max(leaf.max_count() for leaf in self.leaves)
+
+    def max_depth(self):
+        return max(leaf.max_depth() for leaf in self.leaves) + 1
+
+    def __iter__(self):
+        for leaf in self.leaves:
+            for point in leaf:
+                yield point
